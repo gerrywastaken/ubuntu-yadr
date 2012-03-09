d714dbee5bde3c66920202ed5019447261abb400
Renoir Boulanger <renoir.boulanger@ericsson.com>
2012-03-08 23:22:50 -0500

    Merging old repository prefs

---
+++ system/etc/profile.d/rubygems1.8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Ruby Gems local
+# Make sure it is sudo chmod a+x /etc/profile.d/rubygems1.8.sh
+# See: http://groups.google.com/group/compass-users/browse_thread/thread/fc72946d45bf7db5?pli=1
+PATH="$PATH:/var/lib/gems/1.8/bin:$HOME/.gem/ruby/1.8/bin"
---
+++ system/etc/profile.d/svn-meld.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+alias sd='svn diff --diff-cmd /usr/bin/svn-meld'
